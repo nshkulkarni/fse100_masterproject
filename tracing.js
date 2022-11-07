6641e36d146c5a98a79fc77ed6e274cb1b8967be
nshkulkarni <105520469+nshkulkarni@users.noreply.github.com>
2022-11-07 15:06:48 -0700

    Update tracing.js

--- tracing.js
+++ tracing.js
@@ -5,16 +5,24 @@ var smallCircle;
 var largeCircle;
 let timeLimit = 10;
 let countDown;
+var startBox;
+var endBox;
 
 function setup() {
   createCanvas(400, 400);
   frameRate(30);
   textSize(20);
   text("Trace the given shape", width/3.5, height-325);
+  textSize(13);
+  text("Start", 160, 105);
+  text("End",213,105);
   largeCircle = circle(ballx, bally, ballSize*1.2);
   smallCircle = circle(ballx, bally, ballSize);
-  
-}
+  fill("green");
+  startBox = rect(193, 95, 5, 30);
+  fill("red");
+  endBox = rect(203, 95, 5, 30);
+  }
 
 function draw() {
   
@@ -28,7 +36,15 @@ function draw() {
     }
     else 
       {
-        text("Game Over", width/ 2-50, height/2);
+        textSize(60);
+        fill("red");
+        text("Game Over", width/ 2-150, height/2);
+      }
+    if(mouseX > 203 && mouseX < 210 && mouseY > 110 && mouseY < 130)
+      {
+        textSize(75);
+        fill("blue");
+        text("You win!", width/2 -150, height/2);
       }
   }
 }

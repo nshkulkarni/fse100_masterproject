5fb26808d8386e53d3788ba854862290b4b5860b
nshkulkarni <105520469+nshkulkarni@users.noreply.github.com>
2022-11-11 20:57:11 -0700

    Update mainMenu.js

--- mainMenu.js
+++ mainMenu.js
@@ -8,6 +8,9 @@ let screen = 0;
 function setup() 
 {
    mainMenu();
+   button1.mousePressed(gotolink1);
+   button2.mousePressed(gotolink2);
+   button3.mousePressed(gotolink3);
 
 }
 
@@ -15,22 +18,23 @@ function mainMenu()
 {
    createCanvas(400, 400);
   //background color
-  background('teal');
+   let c = color(215, 189, 226);
+  background(c);
+  let o = color(165, 105, 189);
   
   //main text
-  fill('white');
+  fill(color(160, 64, 0));
   textSize(45);
   textFont('Serif');
   text('Rapid Recovery', 55, 140);
   
   //subheader
-  fill('white');
+  
   textSize(17);
   textFont('Serif');
   text('Start your journey to better fine motor skills today!', 30, 172);
   
   //ask user
-  fill('white');
   textSize(14);
   textFont('serif');
   text('Which game would you like to play?', 100, 240);
@@ -38,114 +42,43 @@ function mainMenu()
   //create buttons
   button1 = createButton('Maze');
   button1.show();
-  button1.style('background-color', 'gray');
+  button1.style('background-color', o);
   button1.size(80, 40);
   button1.position(60, 260);
-  button1.mousePressed(game1);
 
   button2 = createButton('Tracing');
   button2.show();
-  button2.style('background-color', 'gray');
+  button2.style('background-color', o);
   button2.size(80, 40);
   button2.position(160, 260);
-  button2.mousePressed(game2);
+
   
   button3 = createButton('Tapping');
   button3.show();
-  button3.style('background-color', 'gray');
+  button3.style('background-color', o);
   button3.size(80, 40);
   button3.position(260, 260);
-  button3.mousePressed(game3);
+
   
   button4 = createButton('Back to Home');
   button4.hide();
   button4.size(80, 30);
   button4.position(10, 20);
   button4.mousePressed(mainMenu);
+  button4.hide();
   
   
   
 }
 
-
-function gameScreen()
-{
-  switch(screen)
-    {
-      //menu
-      case 0:
-        
-        break;
-        
-      //tapping game  
-      case 1:
-        screen1();
-  
-        break;
-        
-      case 2:
-        screen2();
-        
-        break;
-        
-        case 3:
-          screen3();
-        
-        break;
-    }
+function gotolink1() {
+  window.open('https://editor.p5js.org/cnmoore4/sketches/TG5D_UWmQ');
 }
 
-function screen1()
-{
-          button1.hide();
-          button2.hide();
-          button3.hide();
-          button4.show();
-        
-          background('purple');
-          text('enter game 1 here!', 200, 200);
+function gotolink2() {
+  window.open('https://editor.p5js.org/nkulka13/sketches/JMwR8xjQr');
 }
 
-function screen2()
-{
-          button1.hide();
-          button2.hide();
-          button3.hide();
-          button4.show();
-        
-          background('purple');
-          text('enter game 2 here!', 200, 200);
+function gotolink3() {
+  window.open('https://editor.p5js.org/nkulka13/sketches/bFSVD7upO');
 }
-
-function screen3()
-{
-          button1.hide();
-          button2.hide();
-          button3.hide();
-          button4.show();
-        
-          background('purple');
-          text('enter game 3 here!', 200, 200);
-}
-
-function game1()
-{
-  clear();
-  screen = 1;
-  gameScreen();
-}
-
-function game2()
-{
-  clear();
-  screen = 2;
-  gameScreen();
-}
-
-function game3()
-{
-  clear();
-  screen = 3;
-  gameScreen();
-}
-
